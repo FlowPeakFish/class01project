024fd990d75a58c823f1ad43360bc42cc8e87696
FlowPeakFish <i@liufengyu.cn>
2022-09-16 11:40:26 +0800

    update

---
+++ app/src/main/java/cn/fengyu/class01project/DetailActivity.java
@@ -0,0 +1,47 @@
+package cn.fengyu.class01project;
+
+import android.content.Intent;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.time.LocalDate;
+
+public class DetailActivity extends AppCompatActivity {
+
+    DatePicker datePicker;
+
+    TextView textView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        int age = intent.getIntExtra("age",8);
+
+        datePicker = findViewById(R.id.dataPicker1);
+        textView1 = findViewById(R.id.textView1);
+
+        textView1.setText(name + "你好，当前年龄：" +
+                age + "岁");
+
+        LocalDate localDate = LocalDate.now();
+
+        datePicker.init(localDate.getYear(),
+                localDate.getMonthValue() - 1,
+                localDate.getDayOfMonth(),
+                new DatePicker.OnDateChangedListener() {
+                    @Override
+                    public void onDateChanged(DatePicker datePicker,
+                                              int year, int month, int dayOfMonth) {
+                        textView1.setText(year + "年" +
+                                (month + 1) + "月" +
+                                dayOfMonth + "日");
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/cn/fengyu/class01project/MainActivity.java
@@ -0,0 +1,81 @@
+package cn.fengyu.class01project;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+// public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // 1、定义
+    ImageView imageView;
+    TextView textView1;
+    TextView textView2;
+    TextView textView3;
+    EditText editText1;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 2、绑定
+        imageView = findViewById(R.id.imageView1);
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        editText1 = findViewById(R.id.editText1);
+        button = findViewById(R.id.button);
+
+        // 3、使用
+        Log.i("MainActivity", button.getText().toString());
+
+        // button监听器 方法一
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // imageView.setImageResource(R.drawable.shizi);
+                // Intent i = new Intent(
+                //         MainActivity.this,
+                //         DetailActivity.class);
+                // i.putExtra("name", editText1.getText().toString());
+                // // i.putExtra("age", 18);
+                // startActivity(i);
+
+                // Intent intent = new Intent(Intent.ACTION_VIEW);
+                // intent.setData(Uri.parse("https://www.baidu.com"));
+                // startActivity(intent);
+
+                Uri uri = Uri.parse("tel:10086");
+                Intent it = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(it);
+
+                // 返回数据给上一个活动
+                // 书P:72
+            }
+        });
+
+        // button监听器 方法二
+        // button.setOnClickListener(this);
+    }
+
+    // button监听器 方法二
+    // @Override
+    // public void onClick(View view) {
+    //     switch (view.getId()) {
+    //         case R.id.button:
+    //             textView3.setText(editText1.getText().toString());
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // }
+}
