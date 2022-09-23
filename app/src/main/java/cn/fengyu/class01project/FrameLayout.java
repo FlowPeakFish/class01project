13b4fdf44991f26ce69f83b20d977da7ebeabccb
FlowPeakFish <i@liufengyu.cn>
2022-09-23 11:18:21 +0800

    3.1 - 3.8

---
+++ app/src/main/java/cn/fengyu/class01project/FrameLayout.java
@@ -0,0 +1,13 @@
+package cn.fengyu.class01project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FrameLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+    }
+}
