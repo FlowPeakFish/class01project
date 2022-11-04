7039cb57c814cee10869ac0ee92ab429704bb315
FlowPeakFish <i@liufengyu.cn>
2022-11-04 11:23:43 +0800

    FruitListView Step - 2

--- app/src/main/java/cn/fengyu/class01project/adapter/FruitAdapter.java
+++ app/src/main/java/cn/fengyu/class01project/adapter/FruitAdapter.java
@@ -23,20 +23,59 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        final View view;
+        // final ImageView image;
+        // final TextView name;
+        // final TextView price;
+        ViewHolder viewHolder;
+
+        // 为每个子项加载设定的布局
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.fruit_item, parent, false);
+
+            // 从布局中分别获取图片、名称、价格的对象(控件)
+            // image = view.findViewById(R.id.fruit_id);
+            // name = view.findViewById(R.id.fruit_name);
+            // price = view.findViewById(R.id.fruit_price);
+
+            viewHolder = new ViewHolder();
+            viewHolder.image = view.findViewById(R.id.fruit_id);
+            viewHolder.name = view.findViewById(R.id.fruit_name);
+            viewHolder.price = view.findViewById(R.id.fruit_price);
+
+            // view.setTag(R.id.fruit_id,image);
+            // view.setTag(R.id.fruit_name,name);
+            // view.setTag(R.id.fruit_price,price);
+
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+
+            // image = (ImageView) view.getTag(R.id.fruit_id);
+            // name = (TextView) view.getTag(R.id.fruit_name);
+            // price = (TextView) view.getTag(R.id.fruit_price);
+
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
         // 得到对应位置的实体
         Fruit fruit = getItem(position);
-        // 为每个子项加载设定的布局
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.fruit_item,
-                parent, false);
-        // 从布局中分别获取图片、名称、价格的对象(控件)
-        ImageView image = view.findViewById(R.id.fruit_id);
-        TextView name = view.findViewById(R.id.fruit_name);
-        TextView price = view.findViewById(R.id.fruit_price);
         // 分别设置具体的图片和值
-        image.setImageResource(fruit.getImageId());
-        name.setText(fruit.getName());
-        price.setText(fruit.getPrice());
+        // image.setImageResource(fruit.getImageId());
+        // name.setText(fruit.getName());
+        // price.setText(fruit.getPrice());
+
+        viewHolder.image.setImageResource(fruit.getImageId());
+        viewHolder.name.setText(fruit.getName());
+        viewHolder.price.setText(fruit.getPrice());
 
         return view;
+    }
+
+    private class ViewHolder {
+        ImageView image;
+        TextView name;
+        TextView price;
     }
 }
