cac14c41d41a3c5e4e4e3a6d730af9f937f4b15c
FlowPeakFish <i@liufengyu.cn>
2022-11-04 09:03:31 +0800

    SimpleListView

--- app/src/main/java/cn/fengyu/class01project/SimpleListViewActivity.java
+++ app/src/main/java/cn/fengyu/class01project/SimpleListViewActivity.java
@@ -1,12 +1,15 @@
 package cn.fengyu.class01project;
 
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 public class SimpleListViewActivity extends AppCompatActivity {
 
+    // 1、定义对象(控件)
     ListView listView;
 
     @Override
@@ -14,34 +17,45 @@ public class SimpleListViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_list_view);
 
-        listView = findViewById(R.id.ls1);
+        // 2、绑定控件
+        listView = findViewById(R.id.ls2);
 
+        // 3、准备数据
+        //      a. 代码初始化数据
+        //      b. 服务器接口获取数据（RestFul）
+        //      c. 本地存储空间
         String[] list = new String[]{
                 "步茜中",
                 "危帛彦",
-                "童妍倩",
-                "林嘉信",
-                "屈岚彦",
-                "晁璟雨",
-                "虞休芳",
-                "郏桂萱",
-                "项云祜",
-                "尉迟凡荣",
-                "暨震泽",
-                "钟离桂薇",
-                "司徒妮琛",
-                "彭昕帛",
-                "蓝雨轩",
-                "凤柏日",
-                "毕锋听",
-                "班雯芙",
-                "段坤姿",
-                "宇文暄沛"
+                "童妍倩"
         };
 
+        // 4、设计子布局
+        int childItemLayout = android.R.layout.simple_list_item_1;
+        // 5、初始化适配器(子项布局+数据)
         ArrayAdapter<String> adapter = new ArrayAdapter<>(SimpleListViewActivity.this,
-                android.R.layout.simple_list_item_1, list);
+                childItemLayout, list);
 
+        // 6、将适配器加到控件中(子项布局+数据+列表控件)
         listView.setAdapter(adapter);
+
+        // 7、ListView的单击事件
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String name;
+                // 方式一：通过控件获取值
+                TextView textView = (TextView) view;
+                name = textView.getText().toString();
+
+                // 方式二：通过索引值获取值
+                name = list[position];
+
+                Toast.makeText(SimpleListViewActivity.this,
+                                // R.string.clicked_text,)
+                                name + "子项被点击了", LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 }
